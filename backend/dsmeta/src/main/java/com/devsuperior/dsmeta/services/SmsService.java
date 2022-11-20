199c08fe3a93ef4bb5e022a8100dabd828a8a3cb
Talita Rocha <tali_talitarocha@hotmail.com>
2022-11-19 23:12:24 -0300

    Axios, useEffect first request

--- backend/dsmeta/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,8 +34,8 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-				+ " com um total de R$ " + String.format("%.2f", sale.getAmount()) + " em vendas!";
+		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em" + date
+                + " com um total de R$ " + sale.getAmount();
 		
 		Twilio.init(twilioSid, twilioKey);
 
